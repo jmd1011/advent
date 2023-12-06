8740789b335e1a88c50ad5ed01c398b74d84d120
James Decker <jmd1011@gmail.com>
2023-12-05 20:53:21 -0700

    Format

--- 2023/scala/day01.scala
+++ 2023/scala/day01.scala
@@ -1,38 +1,39 @@
 import scala.io.Source
 
 object Day1 extends App {
-    def part1(lines: Iterator[String]): Int = {
-        lines.map{ x =>
-            val digits = x.filter(_.isDigit)
-            digits.head.asDigit * 10 + digits.last.asDigit
-        }.sum
+  def part1(lines: Iterator[String]): Int = {
+    lines.map { x =>
+      val digits = x.filter(_.isDigit)
+      digits.head.asDigit * 10 + digits.last.asDigit
+    }.sum
+  }
+
+  def part2(lines: Iterator[String]): Int = {
+    def getNum(num: String): Int = {
+      val nums = Map(
+        "one" -> 1,
+        "two" -> 2,
+        "three" -> 3,
+        "four" -> 4,
+        "five" -> 5,
+        "six" -> 6,
+        "seven" -> 7,
+        "eight" -> 8,
+        "nine" -> 9
+      )
+
+      if (num(0).isDigit)
+        num(0).asDigit
+      else
+        nums(num)
     }
+    val regex = "(?=([1-9]|one|two|three|four|five|six|seven|eight|nine))".r
+    lines.map { x =>
+      val digits = regex.findAllMatchIn(x).map(_.group(1)).map(getNum).toList
+      digits.head * 10 + digits.last
+    }.sum
+  }
 
-    def part2(lines: Iterator[String]): Int = {
-        def getNum(num: String): Int = {
-            val nums = Map(
-                "one" -> 1,
-                "two" -> 2,
-                "three" -> 3,
-                "four" -> 4,
-                "five" -> 5,
-                "six" -> 6,
-                "seven" -> 7,
-                "eight" -> 8,
-                "nine" -> 9)
-
-            if (num(0).isDigit)
-                num(0).asDigit
-            else
-                nums(num)
-        }
-        val regex = "(?=([1-9]|one|two|three|four|five|six|seven|eight|nine))".r
-        lines.map{ x =>
-            val digits = regex.findAllMatchIn(x).map(_.group(1)).map(getNum).toList
-            digits.head * 10 + digits.last
-        }.sum
-    }
-
-    println(part1(Source.fromFile("2023/input/day01/real").getLines()))
-    println(part2(Source.fromFile("2023/input/day01/real").getLines()))
+  println(part1(Source.fromFile("2023/input/day01/real").getLines()))
+  println(part2(Source.fromFile("2023/input/day01/real").getLines()))
 }
--- 2023/scala/day02.scala
+++ 2023/scala/day02.scala
@@ -1,34 +1,57 @@
 import scala.io.Source
 
 object Day2 extends App {
-    class Entry(val num: Int, val color: String)
-    class Score(val red: Int, val green: Int, val blue: Int)
-    val start = new Score(0, 0, 0)
+  class Entry(val num: Int, val color: String)
+  class Score(val red: Int, val green: Int, val blue: Int)
+  val start = new Score(0, 0, 0)
 
-    def turn(t: String): Score = {
-        t.split(",").map(_.trim).map(x => x.split(" ")).map(x => new Entry(x.head.toInt, x.last)).foldLeft(start)((m, e) => {
-            if (e.color == "red") new Score(math.max(m.red, e.num), m.green, m.blue)
-            else if (e.color == "green") new Score(m.red, math.max(m.green, e.num), m.blue)
-            else new Score(m.red, m.green, math.max(m.blue, e.num))
-        })
-    }
+  def turn(t: String): Score = {
+    t.split(",")
+      .map(_.trim)
+      .map(x => x.split(" "))
+      .map(x => new Entry(x.head.toInt, x.last))
+      .foldLeft(start)((m, e) => {
+        if (e.color == "red") new Score(math.max(m.red, e.num), m.green, m.blue)
+        else if (e.color == "green")
+          new Score(m.red, math.max(m.green, e.num), m.blue)
+        else new Score(m.red, m.green, math.max(m.blue, e.num))
+      })
+  }
 
-    def play(turns: String)(calcPoints: Score => Int): Int = {
-        calcPoints(turns.split(";").map(turn).foldLeft(start)((m, e) => new Score(math.max(m.red, e.red), math.max(m.green, e.green), math.max(m.blue, e.blue))))
-    }
+  def play(turns: String)(calcPoints: Score => Int): Int = {
+    calcPoints(
+      turns
+        .split(";")
+        .map(turn)
+        .foldLeft(start)((m, e) =>
+          new Score(
+            math.max(m.red, e.red),
+            math.max(m.green, e.green),
+            math.max(m.blue, e.blue)
+          )
+        )
+    )
+  }
 
-    def doWork(lines: Iterator[String])(calcPoints: Int => (Score => Int)): Int = {
-        lines.map(line => {
-            val parts = line.split(":")
-            val turns = parts.last.trim()
-            val id = parts.head.split(" ").last.toInt
-            play(turns)(calcPoints(id))
-        }).sum
-    }
+  def doWork(
+      lines: Iterator[String]
+  )(calcPoints: Int => (Score => Int)): Int = {
+    lines
+      .map(line => {
+        val parts = line.split(":")
+        val turns = parts.last.trim()
+        val id = parts.head.split(" ").last.toInt
+        play(turns)(calcPoints(id))
+      })
+      .sum
+  }
 
-    def part1(lines: Iterator[String]): Int = doWork(lines)(id => x => if (x.red <= 12 && x.green <= 13 && x.blue <= 14) id else 0)
-    def part2(lines: Iterator[String]): Int = doWork(lines)(id => x => x.red * x.green * x.blue)
+  def part1(lines: Iterator[String]): Int = doWork(lines)(id =>
+    x => if (x.red <= 12 && x.green <= 13 && x.blue <= 14) id else 0
+  )
+  def part2(lines: Iterator[String]): Int =
+    doWork(lines)(id => x => x.red * x.green * x.blue)
 
-    println(part1(Source.fromFile("2023/input/day02/real").getLines()))
-    println(part2(Source.fromFile("2023/input/day02/real").getLines()))
+  println(part1(Source.fromFile("2023/input/day02/real").getLines()))
+  println(part2(Source.fromFile("2023/input/day02/real").getLines()))
 }
