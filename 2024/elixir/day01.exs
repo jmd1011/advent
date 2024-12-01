56456c9343cb490db50cd51093a87c3ac5b065f1
James Decker <jmd1011@gmail.com>
2024-12-01 16:56:58 +0000

    [2024] Day 1

---
+++ 2024/elixir/day01.exs
@@ -0,0 +1,30 @@
+defmodule Day01 do
+    def part1(l, r) do
+        List.zip([l, r]) \
+            |> Enum.map(fn {a, b} -> abs(a - b) end)\
+            |> Enum.sum
+    end
+
+    def part2(l, r) do
+        l \
+            |> Enum.map(fn x -> x * Enum.count(r, &(&1 == x)) end) \
+            |> Enum.sum
+    end
+end
+
+lines = File.stream!("../input/day01") \
+    |> Enum.map(&String.trim/1) \
+    |> Enum.map(&String.split/1)
+
+l = lines |> Enum.map(&hd/1) \
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sort
+
+r = lines |> Enum.map(&tl/1) \
+    |> Enum.map(&hd/1) \
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sort
+
+IO.puts Day01.part1(l, r)
+IO.puts Day01.part2(l, r)
+
