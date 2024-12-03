bda22e6ba941a3b6cfb06967c2eab561c866b170
James Decker <jmd1011@gmail.com>
2024-12-03 11:24:15 +0000

    [2024] Day 3 complete

--- 2024/elixir/day03.exs
+++ 2024/elixir/day03.exs
@@ -4,12 +4,31 @@ defmodule Day03 do
   end
 
   def part1(lines) do
-    lines
-    |> (&Regex.scan(~r/mul\((\d+),(\d+)\)/, &1))
-    |> Enum.map(fn [_, x, y] -> x * y end)
+    Regex.scan(~r/mul\((\d+),(\d+)\)/, lines)
+    |> Enum.map(fn [_, x, y] -> String.to_integer(x) * String.to_integer(y) end)
     |> Enum.sum()
+  end
+
+  def part2(lines) do
+    pieces =
+      String.split(lines, ~r/don't/)
+      |> Enum.map(&String.split(&1, ~r/do/))
+
+    p = part1(hd(hd(pieces)))
+
+    p +
+      (tl(pieces)
+       |> Enum.map(fn matches ->
+         if length(matches) > 1 do
+           Enum.drop(matches, 1) |> Enum.join() |> part1()
+         else
+           0
+         end
+       end)
+       |> Enum.sum())
   end
 end
 
 lines = Day03.parse()
 IO.puts(Day03.part1(lines))
+IO.puts(Day03.part2(lines))
