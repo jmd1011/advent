fb685f8b5942c49c61b67dd0225b1d8e8363af16
James Decker <jmd1011@gmail.com>
2024-12-05 17:17:29 +0000

    [2024] Day 5 WIP

--- 2024/elixir/day05.exs
+++ 2024/elixir/day05.exs
@@ -6,31 +6,44 @@ defmodule Day05 do
 
     rules
     |> Enum.each(fn rule ->
-      [source, sink] =
-        String.split(rule, "|")
-        |> Enum.map(fn v ->
-          if !:digraph.vertex(graph, v) do
-            :digraph.add_vertex(graph, v)
-          end
+      [source, sink] = String.split(rule, "|")
 
-          :digraph.vertex(graph, v)
-        end)
-
+      source = :digraph.add_vertex(graph, source)
+      sink = :digraph.add_vertex(graph, sink)
       :digraph.add_edge(graph, source, sink)
     end)
 
     updates =
       updates
       |> Enum.map(fn update ->
-        update |> String.split(",") |> Enum.reverse()
+        update |> String.split(",")
       end)
 
     {graph, updates}
   end
 
   def part1({graph, updates}) do
-    :digraph.vertices(graph) |> Enum.map(&IO.inspect/1)
+    updates
+    |> Enum.filter(&correct?(graph, &1))
+    |> Enum.map(&middle(&1, 0, div(length(&1), 2)))
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
+
+  defp middle(l, curr, curr), do: hd(l)
+  defp middle([_ | t], curr, tgt), do: middle(t, curr + 1, tgt)
+
+  defp correct?(graph, [page | pages]) do
+    pages
+    |> Enum.all?(fn succ ->
+      case :digraph.get_path(graph, succ, page) do
+        false -> true
+        _ -> false
+      end
+    end) and correct?(graph, pages)
+  end
+
+  defp correct?(_, []), do: true
 end
 
 Day05.parse("../input/day05") |> tap(&IO.inspect(Day05.part1(&1)))
