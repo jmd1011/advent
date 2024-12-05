08cf94c1902656d177526a57a91aa23b220eb2a1
James Decker <jmd1011@gmail.com>
2024-12-05 19:29:34 +0000

    Working

--- 2024/elixir/day05.exs
+++ 2024/elixir/day05.exs
@@ -24,7 +24,7 @@ defmodule Day05 do
 
   def part1({graph, updates}) do
     updates
-    |> Enum.filter(&correct?(graph, &1))
+    |> Enum.filter(&correct?(graph, &1, MapSet.new()))
     |> Enum.map(&middle(&1, 0, div(length(&1), 2)))
     |> Enum.map(&String.to_integer/1)
     |> Enum.sum()
@@ -33,17 +33,14 @@ defmodule Day05 do
   defp middle(l, curr, curr), do: hd(l)
   defp middle([_ | t], curr, tgt), do: middle(t, curr + 1, tgt)
 
-  defp correct?(graph, [page | pages]) do
-    pages
-    |> Enum.all?(fn succ ->
-      case :digraph.get_path(graph, succ, page) do
-        false -> true
-        _ -> false
-      end
-    end) and correct?(graph, pages)
+  defp correct?(graph, [page | pages], seen) do
+    neighbors = :digraph.out_neighbours(graph, page)
+
+    !Enum.any?(neighbors, fn edge -> MapSet.member?(seen, edge) end) and
+      correct?(graph, pages, MapSet.put(seen, page))
   end
 
-  defp correct?(_, []), do: true
+  defp correct?(_, [], _), do: true
 end
 
 Day05.parse("../input/day05") |> tap(&IO.inspect(Day05.part1(&1)))
