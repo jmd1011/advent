6f9137b64bd6deb569edf2a7548bbecbb66c9d31
James Decker <jmd1011@gmail.com>
2024-12-06 18:50:59 +0000

    [2024] Day 6 Part 2

--- 2024/elixir/advent/lib/day06.ex
+++ 2024/elixir/advent/lib/day06.ex
@@ -33,16 +33,29 @@ defmodule Advent.Day06 do
   def part2() do
     grid = parse("../../input/day06")
     {start_pos, _} = grid |> Enum.find(fn {_, c} -> c == "^" end)
-    possibles = step(grid, MapSet.new(), start_pos, {-1, 0}) |> elem(1)
 
-    # possibles
-    [{5, 3}]
-    |> Enum.reduce(0, fn pos, acc ->
-      IO.inspect("Checking position:")
-      IO.inspect(pos)
+    step(grid, MapSet.new(), start_pos, {-1, 0})
+    |> elem(1)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.chunk_every(750)
+    |> Enum.map(fn chunk ->
+      Task.async(fn -> inner(chunk, grid, start_pos) end)
+    end)
+    |> Task.await_many(6_000_000)
+    |> Enum.flat_map(fn x -> x end)
+    |> MapSet.new()
+    |> MapSet.size()
+  end
+
+  defp inner(chunk, grid, start_pos) do
+    chunk
+    |> Enum.reduce(MapSet.new(), fn pos, acc ->
       case step(Map.put(grid, pos, "#"), MapSet.new(), start_pos, {-1, 0}) do
-        {:cyclic, _} -> acc + 1
-        _ -> acc
+        {:cyclic, _} ->
+          MapSet.put(acc, pos)
+
+        _ ->
+          acc
       end
     end)
   end
@@ -66,24 +79,6 @@ defmodule Advent.Day06 do
   end
 
   defp step(grid, seen, pos, dir) do
-    test =
-      seen
-      |> Enum.filter(fn {seen_pos, seen_dir} ->
-        seen_pos == pos
-      end)
-
-    case pos do
-      {6, 4} ->
-        IO.puts("At")
-        IO.inspect(pos)
-        IO.puts("and going")
-        IO.inspect(dir)
-
-        IO.inspect(test)
-        IO.puts("Have we seen this before: #{MapSet.member?(seen, {pos, dir})}")
-      _ -> nil
-    end
-
     cond do
       !Map.has_key?(grid, pos) ->
         {:acyclic, seen}
