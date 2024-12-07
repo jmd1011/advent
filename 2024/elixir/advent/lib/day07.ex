987ffe58bb5eee68663e199f7a01ffe0a2043b2a
James Decker <jmd1011@gmail.com>
2024-12-07 11:44:23 +0000

    100% code reuse

--- 2024/elixir/advent/lib/day07.ex
+++ 2024/elixir/advent/lib/day07.ex
@@ -1,6 +1,7 @@
 defmodule Advent.Day07 do
   def run do
     IO.inspect(part1())
+    IO.inspect(part2())
   end
 
   defp parse() do
@@ -18,19 +19,29 @@ defmodule Advent.Day07 do
     lines = parse()
 
     lines
-    |> Enum.filter(fn {target, nums} -> calibrated?(target, nums) end)
+    |> Enum.filter(fn {target, nums} -> calibrated?(target, nums, false) end)
     |> Enum.map(&elem(&1, 0))
     |> Enum.sum()
   end
 
-  defp calibrated?(target, nums), do: calibrated?(0, target, nums)
-  defp calibrated?(curr, target, []), do: curr == target
-  defp calibrated?(curr, target, _) when curr > target, do: false
+  def part2() do
+    lines = parse()
 
-  defp calibrated?(curr, target, [num | nums]) do
+    lines
+    |> Enum.filter(fn {target, nums} -> calibrated?(target, nums, true) end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sum()
+  end
+
+  defp calibrated?(target, nums, concat?), do: calibrated?(0, target, nums, concat?)
+  defp calibrated?(curr, target, [], _), do: curr == target
+  defp calibrated?(curr, target, _, _) when curr > target, do: false
+
+  defp calibrated?(curr, target, [num | nums], concat?) do
     concat = ("#{curr}" <> "#{num}") |> String.to_integer()
 
-    calibrated?(curr + num, target, nums) or calibrated?(curr * num, target, nums) or
-      calibrated?(concat, target, nums)
+    calibrated?(curr + num, target, nums, concat?) or
+      calibrated?(curr * num, target, nums, concat?) or
+      (!concat? or calibrated?(concat, target, nums, concat?))
   end
 end
