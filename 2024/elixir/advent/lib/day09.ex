24e69c91ef652450ca733940349441f4d6f27782
James Decker <jmd1011@gmail.com>
2024-12-09 19:01:44 +0000

    [2024] Day 9 Starter

---
+++ 2024/elixir/advent/lib/day09.ex
@@ -0,0 +1,4 @@
+defmodule Advent.Day09 do
+  def run do
+  end
+end
---
+++ 2024/elixir/advent/lib/mix/tasks/day09.ex
@@ -0,0 +1,8 @@
+defmodule Mix.Tasks.Day09 do
+  use Mix.Task
+
+  @moduledoc "Runs Day 09"
+  def run(_) do
+    Advent.Day09.run()
+  end
+end
