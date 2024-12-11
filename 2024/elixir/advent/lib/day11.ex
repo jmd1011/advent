f27e18b8969937bdef8f5baa23f2483163f3304d
James Decker <jmd1011@gmail.com>
2024-12-11 11:58:08 +0000

    [2024] Day 11 Parts 1 & 2

---
+++ 2024/elixir/advent/lib/day11.ex
@@ -0,0 +1,43 @@
+defmodule Advent.Day11 do
+  use Memoize
+
+  def run do
+    Application.ensure_all_started(:memoize)
+    part1() |> IO.inspect()
+    part2() |> IO.inspect()
+  end
+
+  defp parse do
+    File.read!("../../input/day11")
+    |> String.split()
+  end
+
+  def part1() do
+    parse() |> Enum.map(&count(&1, 25)) |> Enum.sum()
+  end
+
+  def part2() do
+    parse() |> Enum.map(&count(&1, 75)) |> Enum.sum()
+  end
+
+  defmemo(count(_, 0), do: 1)
+
+  defmemo count(x, n) do
+    step(x) |> Enum.map(&count(&1, n - 1)) |> Enum.sum()
+  end
+
+  def step(x) do
+    cond do
+      x == "0" ->
+        ["1"]
+
+      rem(String.length(x), 2) == 0 ->
+        x
+        |> String.split_at(div(String.length(x), 2))
+        |> then(fn {a, b} -> [a, Integer.to_string(String.to_integer(b))] end)
+
+      true ->
+        [Integer.to_string(String.to_integer(x) * 2024)]
+    end
+  end
+end
--- 2024/elixir/advent/mix.exs
+++ 2024/elixir/advent/mix.exs
@@ -14,14 +14,15 @@ defmodule Advent.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :memoize]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:arrays, "~> 2.1"}
+      {:arrays, "~> 2.1"},
+      {:memoize, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
