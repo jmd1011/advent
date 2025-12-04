fef8d5e0a31a4a165361a387340cc2c097bffe82
James Decker <jmd1011@gmail.com>
2025-12-04 16:12:51 +0000

    [2025] Cleaning up Day 03

--- 2025/elixir/advent/lib/day03.ex
+++ 2025/elixir/advent/lib/day03.ex
@@ -1,9 +1,8 @@
 defmodule Advent.Day03 do
   def run do
     parse()
-    |> Enum.to_list()
-    |> tap(&IO.puts(part1(&1)))
-    |> tap(&IO.puts(part2(&1)))
+    |> tap(&IO.puts(runner(&1, 2)))
+    |> tap(&IO.puts(runner(&1, 12)))
   end
 
   defp parse do
@@ -14,57 +13,33 @@ defmodule Advent.Day03 do
     end)
   end
 
-  # This was my original solution for part 1
-  # defp iter([h1, h2 | []]), do: h1 * 10 + h2
-
-  # defp iter([h1, h2, h3 | t]) do
-  #   res = max(h1 * 10 + h2, max(h1 * 10 + h3, h2 * 10 + h3))
-  #   iter([div(res, 10)] ++ [rem(res, 10)] ++ t)
-  # end
-
-  defp list_from(l, target_i, target_i), do: l
-  defp list_from([_ | t], i, target_i), do: list_from(t, i + 1, target_i)
-
-  defp find_max_before({9, cur_pos}, _, _, _), do: {9, cur_pos}
-
-  defp find_max_before({cur_val, cur_pos}, _, i, max_i) when i > max_i,
-    do: {cur_val, cur_pos}
-
-  defp find_max_before({cur_val, _}, [h | t], i, max_i) when h > cur_val,
-    do: find_max_before({h, i}, t, i + 1, max_i)
-
-  defp find_max_before(cur, [_ | t], i, max_i), do: find_max_before(cur, t, i + 1, max_i)
-
-  defp iter2(bank, last) do
-    {res, _} =
-      1..last
-      |> Enum.reduce({0, -1}, fn from_end, {acc, i} ->
-        next_start = i + 1
-        n_bank = list_from(bank, 0, next_start)
-        next_end = length(bank) - last + from_end - 1
-
-        {cur_val, cur_pos} =
-          find_max_before(
-            {hd(n_bank), next_start},
-            n_bank,
-            next_start,
-            next_end
-          )
-
-        {acc * 10 + cur_val, cur_pos}
-      end)
-
-    res
+  defp find_max_before(bank, i, max_i) do
+    bank
+    |> Enum.drop(i)
+    |> Enum.take(max_i - i + 1)
+    |> Enum.with_index(i)
+    |> Enum.max_by(fn {val, _} -> val end)
   end
 
-  defp part1(banks) do
-    banks |> Enum.map(&iter2(&1, 2)) |> Enum.sum()
+  defp inner(bank, last) do
+    with bank_len = length(bank) do
+      {res, _} =
+        1..last
+        |> Enum.reduce({0, -1}, fn from_end, {acc, i} ->
+          next_start = i + 1
+          next_end = bank_len - last + from_end - 1
+
+          {cur_val, cur_pos} =
+            find_max_before(bank, next_start, next_end)
+
+          {acc * 10 + cur_val, cur_pos}
+        end)
+
+      res
+    end
   end
 
-  defp part2(banks) do
-    banks
-    |> Enum.map(&iter2(&1, 12))
-    # |> tap(fn jolts -> Enum.each(jolts, fn x -> IO.inspect(x) end) end)
-    |> Enum.sum()
+  defp runner(banks, num) do
+    banks |> Enum.map(&inner(&1, num)) |> Enum.sum()
   end
 end
